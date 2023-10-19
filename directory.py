e3232ccfd3b7e774d48bd44265d08c3718accef5
Bikram998 <alphabikram@gmail.com>
2023-10-20 01:41:51 +0800

    Directory handling in python

---
+++ directory.py
@@ -0,0 +1,17 @@
+# To create a new directory in python we use following command
+import os
+dir_path = "/Users/bikram/Desktop/file/python3"
+os.mkdir(dir_path)
+
+
+# To change the current directory we use 
+
+os.chdir("directory_name")
+
+# to remove/delete the directory we use following command
+os. getcwd()
+print(os.getcwd())
+
+# to remove/delete directory 
+os.rmdir()
+# Note: Before removing the directory we should delete all the content on that directory
